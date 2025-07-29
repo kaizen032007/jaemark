74ab0aba02c7379c17eb1d7dc053345f307b1a1a
kaizen032007 <jaemarkalmeria08@gmail.com>
2025-07-29 13:54:03 +0800

    Add files via upload

---
+++ javaprojects/grades(ternary operators).java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class workspace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ENTER YOUR GRADES: ");
+        int score = scanner.nextInt();
+
+        String results = (score >= 60) ? "PASS" : "FAILED";
+
+        String remark = (score >=90) ? "OUTSTANDING"    
+                        : (score >=80) ? "EXCELLENT"
+                        : (score >=70) ? "GOOD"
+                        : (score >=60) ? "FAIR"
+                        : (score >= 50) ? "FAILED"
+                        : "RESTART AGAIN";
+                        
+        System.out.println("RESULTS ", results);
+        System.out.println("REMARKS " + remark);
+                    
+        scanner.close();
+    }
+}
